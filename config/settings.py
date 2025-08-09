47eaf0348850ef21ddbcef65fc96505ec8d98d56
Richard <richardmakovs08@gmail.com>
2025-08-09 23:46:49 +0200

    added url to front

--- config/settings.py
+++ config/settings.py
@@ -63,7 +63,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://localhost:5173",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://prueba.richardmakov.com/"
 ]
 
 ROOT_URLCONF = 'config.urls'
